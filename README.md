df8cfca1a490e8cf3d64d1562a3330c1bedbddf9
Unkwown-User2 <benjaminjneudorf@gmail.com>
2021-03-31 22:35:31 -0500

    pushed everything

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # 8/16 bit ansi colours
- 8/16 bit ansi colours for C/C++
+<h4>a nice header file for 8/16 bit ansi colors for C/C++ you just have to ```#include``` the file in your file</h4>
---
+++ ansiColours.h
@@ -0,0 +1,50 @@
+//normal colours
+// foreground
+#define BLACK_FG "\033[30m"
+#define RED_FG "\033[31m"
+#define GREEN_FG "\033[32m"
+#define YELLOW_FG "\033[33m"
+#define BLUE_FG "\033[34m"
+#define MAGENTA_FG "\033[35m"
+#define CYAN_FG "\033[36m"
+#define WHITE_FG "\033[37m"
+// background
+#define BLACK_BG "\033[40m"
+#define RED_BG "\033[41m"
+#define GREEN_BG "\033[42m"
+#define YELLOW_BG "\033[43m"
+#define BLUE_BG "\033[44m"
+#define MAGENTA_BG "\033[45m"
+#define CYAN_BG "\033[46sm"
+#define WHITE_BG "\033[47m"
+
+//bright colours
+// foreground
+#define BRT_BLACK_FG "\033[90m"
+#define BRT_RED_FG "\033[91m"
+#define BRT_GREEN_FG "\033[92m"
+#define BRT_YELLOW_FG "\033[93m"
+#define BRT_BLUE_FG "\033[94m"
+#define BRT_MAGENTA_FG "\033[95m"
+#define BRT_CYAN_FG "\033[96m"
+#define BRT_WHITE_FG "\033[97m"
+// background
+#define BRT_BLACK_BG "\033[100m"
+#define BRT_RED_BG "\033[101m"
+#define BRT_GREEN_BG "\033[102m"
+#define BRT_YELLOW_BG "\033[103m"
+#define BRT_BLUE_BG "\033[104m"
+#define BRT_MAGENTA_BG "\033[105m"
+#define BRT_CYAN_BG "\033[106m"
+#define BRT_WHITE_BG "\033[107m"
+
+//misc
+#define RESET "\033[0m" //resets all ansi things to the defaults
+// misc on
+#define BOLD "\033[1m" //makes it bold and brighter
+#define UNDERLINE "\033[4m" //adds an underline to the text the same colour as text
+#define INVERSE "\033[m" //swaps the fg and bg colours
+// misc off
+#define BOLD_OFF "\033[21m" //turns off bold
+#define UNDERLINE_OFF "\033[24m" // turns off underline
+#define INVERSE_OFF "\033[27m" // turns off inverse
